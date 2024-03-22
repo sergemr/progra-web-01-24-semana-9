f7bc4f7918fb8cd5451348b4a080db376f414219
Sergio Morales <sergemr@gmail.com>
2024-03-21 20:05:42 -0600

    filter users

--- front-end/src/components/users/users.js
+++ front-end/src/components/users/users.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import User from "../user/user";
 import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
 //import Item from "@mui/material/Grid";
 const Users = () => {
   const [users, setUsers] = useState([]);
@@ -23,16 +24,38 @@ const Users = () => {
     fetchUsers();
   }, []);
 
+  const handleFilter = (e) => {
+    const { value } = e.target;
+    const filteredUsers = users.filter((user) => {
+      return user.user_name.toLowerCase().includes(value.toLowerCase());
+    });
+    setUsers(filteredUsers);
+  };
   return (
     <div className={styles.Users} data-testid="Users">
       Usuarios Registrados: &nbsp;
       {users.length}
       <Grid container>
-        {users.map((user) => (
+        <Grid spacing={2} item xs={12}>
+          <TextField
+            id="outlined-basic"
+            onChange={handleFilter}
+            label="Outlined"
+            variant="outlined"
+          />
+        </Grid>
+        <hr />
+      </Grid>
+      <Grid container>
+        {users.map((user, index) => (
           <Grid item xs={12} md={6} lg={4}>
             <User user={user} refreshData={fetchUsers} />
           </Grid>
         ))}
+
+        {users.forEach((user) => {
+          console.log("user: forEach", user);
+        })}
       </Grid>
       <ul></ul>
     </div>
